b640671042b5cf5333e172b256b902887ccd010b
Ed Vincent Patulin <patulinedvincent@gmail.com>
2023-10-26 03:03:25 +0800

    Added User Request and Store API

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\UserRequest;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 use App\Models\User;
 
 class UserController extends Controller
@@ -19,6 +21,19 @@ class UserController extends Controller
     /**
      * Display the specified resource.
      */
+
+     public function store(UserRequest $request)
+     {
+         //Retrieve the validated input data...
+         $validated = $request->validated();
+
+         $validated['password'] = Hash::make($validated['password']);
+ 
+         $user = User::create($validated);
+ 
+         return $user;
+     }
+
     public function show(string $id)
     {
         return User::findOrFail($id);
