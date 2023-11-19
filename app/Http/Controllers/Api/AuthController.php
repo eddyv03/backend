b07fd7e3f1086584d8ab7c7a3d03b89b487d0ffd
Ed Vincent Patulin <patulinedvincent@gmail.com>
2023-11-19 23:55:58 +0800

    Added Logout API and Correct Route groups

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api;
 
 use App\Models\User;
+use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\UserRequest;
 use Illuminate\Support\Facades\Hash;
@@ -36,9 +37,15 @@ class AuthController extends Controller
      * Logout the specified resource.
      */
 
-     public function logout()
+     public function logout(Request $request)
      {
-        return false;
+        $request->user()->tokens()->delete();
+
+        $response = [
+            'message'   => 'Logout.'
+        ];
+
+        return $response;
      }
     
 }
--- routes/api.php
+++ routes/api.php
@@ -18,39 +18,48 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::controller(AuthController::class)->group(function () {
-    Route::post('/login', 'login')->name('user.login');
-    Route::post('/logout', 'logout');
+//Public APIs
+Route::post('/login', [AuthController::class, 'login'])->name('user.login');
+Route::post('/user', [UserController::class, 'store'])->name('user.store');
+
+
+//Private APIs
+Route::middleware(['auth:sanctum'])->group(function () {
+    Route::post('/logout', [AuthController::class, 'logout']);
+
+    Route::controller(CarouselItemsController::class)->group(function () {
+        Route::get('/carousel', 'index');
+        Route::get('/carousel/{id}', 'show');
+        Route::post('/carousel', 'store');
+        Route::put('/carousel/{id}', 'update');
+        Route::delete('/carousel/{id}', 'destroy');
+    });
+
+    Route::controller(UserController::class)->group(function () {
+        Route::get('/user', 'index');
+        Route::get('/user/{id}', 'show');
+        Route::put('/user/{id}', 'update')->name('user.update');
+        Route::put('/user/email/{id}', 'email')->name('user.email');
+        Route::put('/user/password/{id}', 'password')->name('user.password');
+        Route::delete('/user/{id}', 'destroy');
+    });
+
+    Route::controller(LetterController::class)->group(function () {
+        Route::get('/letter', 'index');
+        Route::get('/letter/{id}', 'show');
+        Route::post('/letter', 'store');
+        Route::delete('/letter/{id}', 'destroy');
+    }); 
+
 });
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
 
-Route::controller(CarouselItemsController::class)->group(function () {
-    Route::get('/carousel', 'index');
-    Route::get('/carousel/{id}', 'show');
-    Route::post('/carousel', 'store');
-    Route::put('/carousel/{id}', 'update');
-    Route::delete('/carousel/{id}', 'destroy');
-});
 
-// Route::controller(UserController::class)->group(function () {
-//     Route::get('/user', 'index');
-//     Route::get('/user/{id}', 'show');
-//     Route::post('/user', 'store')->name('user.store');
-//     Route::put('/user/{id}', 'update')->name('user.update');
-//     Route::put('/user/email/{id}', 'email')->name('user.email');
-//     Route::put('/user/password/{id}', 'password')->name('user.password');
-//     Route::delete('/user/{id}', 'destroy');
-// });
 
-Route::controller(LetterController::class)->group(function () {
-    Route::get('/letter', 'index');
-    Route::get('/letter/{id}', 'show');
-    Route::post('/letter', 'store');
-    Route::delete('/letter/{id}', 'destroy');
-});
+
+
+
+
 
 
 
